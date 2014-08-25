65fe202638260d1db0cac918b0ff5f42564ad3b8
Claudio Fior <caiofior@gmail.com>
2014-08-25 17:46:25 +0200

    Setting up db connection

--- config/config.php
+++ config/config.php
@@ -1,2 +1,7 @@
 <?php
-ini_set('include_path',ini_get('include_path').':/usr/share/php/libzend-framework-php');
+ini_set('include_path','.:/usr/share/php/vendor/zendframework/zendframework/library');
+$configArray =array(
+    'database' => array(
+        
+    )
+);
