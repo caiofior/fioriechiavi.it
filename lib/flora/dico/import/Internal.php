06f2190485c86e5b101c1495ae780178a5ae4d50
caiofior <caiofior@gmail.com>
2015-07-22 10:14:01 +0200

    Fixed different number of columns between head and body

--- lib/flora/dico/import/Internal.php
+++ lib/flora/dico/import/Internal.php
@@ -13,6 +13,12 @@ class Internal implements \flora\dico\import\Import {
       $dicoItemColl->emptyColl();
       $cols = array('id','text','taxa_id');
       while($row = fgetcsv($stream,1000,"\t")) {
+         while (sizeof($row)<sizeof($cols)) {
+             array_push($row,'');
+         }
+         while (sizeof($row)>sizeof($cols)) {
+             array_pop($row);
+         }
          $row = array_combine($cols, $row);
          $row['id']=trim($row['id']);
          $dicoItem = $dicoItemColl->addItem();
