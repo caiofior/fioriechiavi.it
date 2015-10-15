fa2ae6381384aeee95e80d413945b9f816a3d082
caiofior <caiofior@gmail.com>
2015-10-15 15:34:01 +0200

    Added taxa map

--- control/map.php
+++ control/map.php
@@ -1,5 +1,5 @@
 <?php
-$this->getTemplate()->setBlock('head','general/head.phtml');
+$this->getTemplate()->setBlock('head','map/head.phtml');
 $this->getTemplate()->setBlock('middle','map/middle.phtml');
 $this->getTemplate()->setBlock('footer','general/footer.phtml');
 
