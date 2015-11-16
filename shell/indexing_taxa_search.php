cd69079676672c682663b7ab381e48b5e946a2a8
caiofior <caiofior@gmail.com>
2015-11-16 13:00:27 +0100

    Indexing: * Removed partial indexing; * Fixed search again; * Show taxa with no names.

--- shell/indexing_taxa_search.php
+++ shell/indexing_taxa_search.php
@@ -3,51 +3,15 @@ echo 'Start at '.date('d/m/Y H:i:s').PHP_EOL;
 if(!array_key_exists('db', $GLOBALS)) {
     require __DIR__.'/../include/pageboot.php';
 }
-if  (
-        $GLOBALS['db']->config->background->useAJAX != true &&
-        is_numeric($GLOBALS['db']->config->background->splitEvery) &&
-        isset($argv) &&
-        is_array($argv) &&
-        !array_key_exists(1, $argv)
-    ) {
-    $statement = $GLOBALS['db']->query('SELECT COUNT(`id`) FROM `taxa`');
-    $resultSet = new \Zend\Db\ResultSet\ResultSet();
-    $resultSet->initialize($statement->execute());    
-    $count = $resultSet->current()['COUNT(`id`)'];
-    $steps = range(0,$count,intval($GLOBALS['db']->config->indexing->splitEvery));
-    foreach ($steps as $step) {
-        echo 'Step '.$step.PHP_EOL;
-        $cmd = 'php '.__FILE__.' '.$step.' 2>&1';
-        echo shell_exec($cmd);
-    }
-    echo 'Memory '.  number_format(memory_get_peak_usage()/1024/1024,2).' M'.PHP_EOL;
-    echo 'End at '.date('d/m/Y H:i:s').PHP_EOL;
-    exit;
-}
-if (
-        !isset($argv) ||
-        !is_array($argv) ||
-        !array_key_exists(1, $argv) ||
-        $argv[1]==0
-        ) {
-    echo 'Cleaned up search table '.date('d/m/Y H:i:s').PHP_EOL;
-    $statement = $GLOBALS['db']->query('TRUNCATE TABLE `taxa_search`');
-    $statement->execute();
-}
-if  (
-        is_numeric($GLOBALS['db']->config->background->splitEvery) &&
-        isset($argv) &&
-        is_array($argv) &&
-        array_key_exists(1, $argv)
-    ) {
-    $statement = $GLOBALS['db']->query('
-        SELECT `id`
-        FROM `taxa` ORDER BY `id` ASC LIMIT '.intval($argv[1]).','.intval($GLOBALS['db']->config->indexing->splitEvery));
-} else {
-    $statement = $GLOBALS['db']->query('
-        SELECT `id`
-        FROM `taxa` ORDER BY `id` ASC');
-}
+
+echo 'Cleaned up search table '.date('d/m/Y H:i:s').PHP_EOL;
+$statement = $GLOBALS['db']->query('TRUNCATE TABLE `taxa_search`');
+$statement->execute();
+
+$statement = $GLOBALS['db']->query('
+    SELECT `id`
+    FROM `taxa` ORDER BY `id` ASC');
+
 $resultSet = new \Zend\Db\ResultSet\ResultSet();
 $resultSet->initialize($statement->execute());
 $taxa = new \flora\taxa\Taxa($GLOBALS['db']);
@@ -80,33 +44,34 @@ do {
         $resultSet->next();
     }
     $id = array_shift($todoIds);
-    $shifts =0;
-       
+    $shifts = 0;
     while (
             $id > 1 &&
-            !in_array($parentIds[$id],$doneIds) &&
-            $shifts++ < count($todoIds)
+            count(array_intersect($parentIds[$id],$doneIds)) == 0 &&
+            $shifts++ <= count($todoIds)
           ) {
-    
         array_push($todoIds, $id);
         $id = array_shift($todoIds);
     }
+    $taxa->loadFromId($id);
     if (
             $id == 1 ||
             count(array_intersect($parentIds[$id],$doneIds)) > 0
         ) {
-        
-        $taxa->loadFromId($id);
         $message = $taxa->updateSearch();
         if ($message != '') {
             echo $message;
         }
         array_unshift($doneIds,$id);
-        
+        $shifts = 0;
+    } else {
+        array_push($todoIds, $id);
     }
-    
-} while (count($todoIds)>0 || $resultSet->valid());
-
+} while ($shifts == 0 || $resultSet->valid());
+foreach ($todoIds as $id ) {
+    $taxa->loadFromId($id);
+    echo 'Taxa '.$taxa->getData('name').' '.$taxa->getData('id').' has no parent'.PHP_EOL;
+}
 $statement = $GLOBALS['db']->query('ALTER TABLE `taxa_search` ENABLE KEYS');
 $statement->execute();
 $statement = $GLOBALS['db']->query('ALTER TABLE `taxa_search_attribute` ENABLE KEYS');
