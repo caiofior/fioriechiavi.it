f714e37a68d37268f81ec1255bf23591aa736bd5
caiofior <caiofior@gmail.com>
2015-11-19 12:43:14 +0100

    Optimized images, restored mysql defaults

---
+++ install/sql/taxa_kind.sql
@@ -0,0 +1,17 @@
+
+-- phpMyAdmin SQL Dump
+-- version 2.8.0.2
+-- http://www.phpmyadmin.net
+-- 
+-- Host: sql.fioriechiavi.it
+-- Generato il: 20 Mar, 2015 at 09:48 AM
+-- Versione MySQL: 5.5.31
+-- Versione PHP: 4.3.10-22
+-- 
+-- Database: `fioriech65618`
+-- 
+
+-- 
+-- Dump dei dati per la tabella `taxa_kind`
+-- 
+INSERT INTO `taxa_kind` (`id`, `name`) VALUES ('1', 'Radice');
