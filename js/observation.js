8ec2dab68bf1cc8145efe3b92d1961746939c686
caiofior <caiofior@gmail.com>
2016-04-18 16:45:23 +0200

    Asinc navigation in observations

--- js/observation.js
+++ js/observation.js
@@ -13,12 +13,26 @@ function updateMap() {
            });
            count = 0;
            $.each(points, function(index, point) {
-              markers[markers.length+1] = new google.maps.Marker({
+              markers[markers.length] = new google.maps.Marker({
                     position: new google.maps.LatLng(point.latitude, point.longitude),
                     map: map,
                     label: String.fromCharCode(65+count++)
               });
-           });
+           });   
         }
+        map.setZoom(radius);
+	map.setCenter(new google.maps.LatLng(centroid.latitude,centroid.longitude));
         $(".fancybox").fancybox();
 }
+$("#observationContent").on("click","#paginationContainer a.pageSelector",function(e){
+    $.ajax({
+        url: $(this).attr("href"),
+        data: {"xhr":1},
+        method:"post",
+        async : false,
+        success : function (data) {
+                $("#observationContent").html(data);
+        }
+    }); 
+    e.preventDefault();
+});
