91cff5e20133b08fc9eeb82260f7f193144b4741
caiofior <caiofior@gmail.com>
2016-04-20 18:24:44 +0200

    Fixed map

--- js/observation.js
+++ js/observation.js
@@ -1,27 +1,29 @@
-function updateMap() {
-        if (typeof google != 'undefined' && $('#map-canvas').length > 0) {
-           if (typeof map == 'undefined') {
-                map = new google.maps.Map($('#map-canvas')[0],{
+if (typeof google != 'undefined' && $('#map-canvas').length > 0 && typeof map == 'undefined' ) {
+latlong = new google.maps.LatLng(centroid.latitude,centroid.longitude);
+map=new google.maps.Map($('#map-canvas')[0],{
                      zoom: radius,
                      scrollwheel: false,
-                     center: new google.maps.LatLng(centroid.latitude,centroid.longitude)
+                     center: latlong
                 });
-                markers = [];
-           }
+markers = [];
+}
+function updateMap() {
+        if (typeof google != 'undefined' && $('#map-canvas').length > 0) {  
            $.each(markers, function(index, marker) {
                marker.setMap(null);
            });
            count = 0;
            $.each(points, function(index, point) {
               markers[markers.length] = new google.maps.Marker({
-                    position: new google.maps.LatLng(point.latitude, point.longitude),
+                   position: new google.maps.LatLng(point.latitude, point.longitude),
                     map: map,
                     label: String.fromCharCode(65+count++)
               });
-           });   
+           });
+	   map.setZoom(radius);
+	   map.setCenter(latlong);
         }
-        map.setZoom(radius);
-	map.setCenter(new google.maps.LatLng(centroid.latitude,centroid.longitude));
+        
         $(".fancybox").fancybox();
 }
 $("#observationContent").on("click","#paginationContainer a.pageSelector",function(e){
