5ef6c2ba2a3263233585586897dd7f3ef0487e00
caiofior <caiofior@gmail.com>
2017-05-29 18:24:51 +0200

    Add more links

--- shell/exportWords.sh
+++ shell/exportWords.sh
@@ -1,1 +1,1 @@
-sudo myisam_ftdump -c /var/lib/mysql/plantboo_main/taxa_search 3 | sort -rn | head -n 1000 | awk '{print $3}' > /home/caiofior/Scaricati/plantbook.it/shell/words.txt
+sudo myisam_ftdump -c /var/lib/mysql/plantboo_main/taxa_search 3 | sort -rn | head -n 5000 | awk '{print $3}' > /home/caiofior/Scaricati/plantbook.it/shell/words.txt
