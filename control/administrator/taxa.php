026415781d499f031ec800e92e8519f74afef08f
caiofior <caiofior@gmail.com>
2019-09-24 10:01:12 +0200

    Fixed not valid utf8 characters il taxa list

--- control/administrator/taxa.php
+++ control/administrator/taxa.php
@@ -34,6 +34,19 @@ if (array_key_exists('sEcho', $_REQUEST)) {
                    }
 
             }
+            $regex = <<<'END'
+/
+  (
+    (?: [\x00-\x7F]                 # single-byte sequences   0xxxxxxx
+    |   [\xC0-\xDF][\x80-\xBF]      # double-byte sequences   110xxxxx 10xxxxxx
+    |   [\xE0-\xEF][\x80-\xBF]{2}   # triple-byte sequences   1110xxxx 10xxxxxx * 2
+    |   [\xF0-\xF7][\x80-\xBF]{3}   # quadruple-byte sequence 11110xxx 10xxxxxx * 3 
+    ){1,100}                        # ...one or more times
+  )
+| .                                 # anything else
+/x
+END;
+            $data = preg_replace($regex, '$1', $data);
             $row[] = $data;
          }
          $result['aaData'][]=$row;
